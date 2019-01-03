1aaecec3c5e9c45d44df87c0890f5e7877d99a8b
Samuel Nadarajan <>
2019-01-03 14:58:45 -0500

    base screen for project form

---
+++ lib/screens/project_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProjectForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Project"),
+      ),
+      body: Center(
+        child: RaisedButton()
+      ),
+    );
+  }
+
+}
